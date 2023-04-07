4087178e1f7b2a5fb1043675d1771478f1a4160d
Websane <websanego@gmail.com>
2023-04-07 16:20:03 +0300

    feat: add send transaction from hardhat acc

--- src/components/UserWallet/UserWallet.tsx
+++ src/components/UserWallet/UserWallet.tsx
@@ -1,12 +1,14 @@
-import { useState, useEffect, FormEvent, ChangeEvent } from 'react';
+import { useState, useEffect, FormEvent, ChangeEvent, useRef } from 'react';
 import { JsonRpcProvider, JsonRpcSigner, Wallet, ethers } from 'ethers';
 
 type AccountInfo = {
   address: string;
   balance: string;
-}
+};
 
-const getAddressBalance = async (provider: JsonRpcProvider, address: string) => {
+const UNIT = 'ETH';
+
+const getAddressBalance = async (address: string, provider: JsonRpcProvider) => {
   const rawBalance = await provider.getBalance(address);
   const balance = ethers.formatEther(rawBalance);
   return balance;
@@ -17,7 +19,7 @@ const getAccountsInfo = async (accounts: JsonRpcSigner[], provider: JsonRpcProvi
 
   for (const account of accounts) {
     const address = account.address;
-    const balance = await getAddressBalance(provider, address);
+    const balance = await getAddressBalance(address, provider);
 
     accountsInfo.push({ address, balance });
   }
@@ -27,31 +29,56 @@ const getAccountsInfo = async (accounts: JsonRpcSigner[], provider: JsonRpcProvi
 
 export const UserWallet = () => {
   const [provider, setProvider] = useState<JsonRpcProvider | null>(null);
-  const [accounts, setAccounts] = useState<JsonRpcSigner[]>([]);
   const [accountsInfo, setAccountsInfo] = useState<AccountInfo[]>([]);
   const [wallet, setWallet] = useState<Wallet | null>(null);
-  const [from, setFrom] = useState('');
+  const [walletBalance, setWalletBalance] = useState('');
+  const [walletAddress, setWalletAddress] = useState('');
   const [to, setTo] = useState('');
   const [value, setValue] = useState(0);
 
   const connectToProvider = async () => {
     const provider = new ethers.JsonRpcProvider('http://localhost:8545'); // Подключаемся к локальному блокчейну
     setProvider(provider);
-    const accounts = await provider.listAccounts();
-    setAccounts(accounts);
-    const privateKey = '0x0123456789012345678901234567890123456789012345678901234567890123'; // Приватный ключ
-    const wallet = new ethers.Wallet(privateKey, provider); // Создаем кошелек на основе приватного ключа и провайдера
+
+    const hardhatAccountPrivateKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
+    const hardhatAccountWallet = new ethers.Wallet(hardhatAccountPrivateKey, provider);
+
+    const userPrivateKey = '0x0123456789012345678901234567890123456789012345678901234567890123';
+    const userWallet = new ethers.Wallet(userPrivateKey, provider);
     setWallet(wallet);
 
-    const fromAddress = await wallet.getAddress(); // Получаем адрес кошелька
-    setFrom(fromAddress);
+    const fromAddress = await userWallet.getAddress();
+
+    setWalletAddress(fromAddress);
+
+    // Отправляем средства с тестового аккаунта на целевой кошелек
+    const tx = await hardhatAccountWallet.sendTransaction({
+      to: fromAddress,
+      value: ethers.parseEther('150'),
+    });
+    await tx.wait();
+  };
+
+  const handleAccountsInfoUpdate = async () => {
+    if (provider) {
+      const accounts = await provider.listAccounts();
+      getAccountsInfo(accounts, provider).then((info) => setAccountsInfo(info));
+    }
+  };
+
+  const handleWalletBalanceUpdate = async () => {
+    if (provider) {
+      getAddressBalance(walletAddress, provider).then((balance) => setWalletBalance(balance));
+    }
   };
 
   useEffect(() => {
-    if (accounts.length > 0 && provider) {
-      getAccountsInfo(accounts, provider).then(info => setAccountsInfo(info))
-    }
-  }, [accounts])
+    handleAccountsInfoUpdate();
+  }, [provider]);
+
+  useEffect(() => {
+    handleWalletBalanceUpdate();
+  }, [walletAddress]);
 
   const handleToChange = (event: ChangeEvent<HTMLInputElement>) => {
     setTo(event.target.value);
@@ -86,13 +113,25 @@ export const UserWallet = () => {
           <p>Connected</p>
           <p>Accounts:</p>
           <ul>
-            {accountsInfo.map(({address, balance}) => (
-              <li key={address}>
+            {accountsInfo.map(({ address, balance }) => (
+              <li key={address} style={{ display: 'grid', maxWidth: '650px', border: '1px solid black' }}>
                 <span>Address: {address}</span>
-                <span>Balance: {balance} ETH</span>
+                <span>
+                  Balance: {balance} {UNIT}
+                </span>
               </li>
             ))}
           </ul>
+          <button onClick={handleAccountsInfoUpdate}>Update info</button>
+        </div>
+      )}
+      {walletAddress && (
+        <div>
+          <p>Your new account: {walletAddress}</p>
+          <p>
+            Balance: {walletBalance} {UNIT}
+          </p>
+          <button onClick={handleWalletBalanceUpdate}>Update your balance</button>
         </div>
       )}
       <form onSubmit={handleSubmit}>
